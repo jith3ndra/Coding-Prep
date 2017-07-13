79f67320a64b6401aaedfa462248222f5784a665
jith3ndra <jithendra.ggr@gmail.com>
2017-07-13 01:12:32 -0700

    modified

--- can_jump.cc
+++ can_jump.cc
@@ -4,12 +4,12 @@
 
     bool canJump(vector<int>& nums) {
         if(!nums.size()) return false;
-        int reach = 0;
-        for(int i=0;i<nums.size()-1;i++){
+        int reach = 0, i;
+        for(i=0;i<nums.size()-1;i++){
             if( i > reach){
                 break;
             }
             reach = max(reach, i+nums[i]);
         }
-        return i==nums.size()-1;
+        return reach >=nums.size()-1;
     }
